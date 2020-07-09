d8703207bb5fe180bf9eea758c6fdd663eb31074
berkaykarakoc <karakocberkay@outlook.com>
2020-07-09 15:42:16 +0300

    error boundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
 class App extends Component {
@@ -24,23 +25,23 @@ class App extends Component {
     };
 
     render() {
-        const {robots, searchfield} = this.state;
+        const { robots, searchfield } = this.state;
         const filteredRobots = robots.filter((robot) => {
-            return robot.name
-                .toLowerCase()
-                .includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
-        return !robots.length ?
-            <h1>Loading</h1> :
-            (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
+        return !robots.length ? (
+            <h1>Loading</h1>
+        ) : (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
                         <CardList robots={filteredRobots} />
-                    </Scroll>
-                </div>
-            );
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+        );
     }
 }
 
